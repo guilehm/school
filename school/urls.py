cf0ad46652f671f3b9e4a4c054716088ce1c1a5f
guilherme <guile.hm@hotmail.com>
2021-05-28 20:32:19 -0300

    update routes

--- school/urls.py
+++ school/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from school.core import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('teachers/', views.teacher_list, name='teacher-list'),
+    path('teachers/<int:pk>/', views.teacher_detail, name='teacher-detail'),
 ]
