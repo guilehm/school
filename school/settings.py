14a6c6856a7c215d79414a8e1e0dbeac457f4764
guilherme <guile.hm@hotmail.com>
2021-05-29 12:12:10 -0300

    install and configure graphql with graphene

--- school/settings.py
+++ school/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'school.core',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPHENE = {
+    'SCHEMA': 'school.api.schema.schema'
+}
--- school/urls.py
+++ school/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
+
 
 from school.core import views
 
@@ -28,6 +31,7 @@ urlpatterns = [
     path('students/<int:pk>/', views.student_detail, name='student-detail'),
     path('teachers/', views.teacher_list, name='teacher-list'),
     path('teachers/<int:pk>/', views.teacher_detail, name='teacher-detail'),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
 
 if settings.DEBUG:
