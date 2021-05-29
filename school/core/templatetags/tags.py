3dfdad9a204146ae5b7d7cf538bfefc316a9928d
guilherme <guile.hm@hotmail.com>
2021-05-28 21:51:47 -0300

    create condition to stars

--- school/core/templatetags/tags.py
+++ school/core/templatetags/tags.py
@@ -1,11 +1,14 @@
 from django import template
 
+from school.core.models import TeacherStudentRelation
+
 register = template.Library()
 
 
 @register.inclusion_tag('core/tags/user-table-tag.html')
-def user_table(instances, col1, col2):
+def user_table(instances, col1, col2=None, instance=None):
     return {
+        'instance': instance,
         'instances': instances,
         'col1': col1,
         'col2': col2,
@@ -15,3 +18,14 @@ def user_table(instances, col1, col2):
 @register.inclusion_tag('core/tags/user-change-form-tag.html')
 def user_change_form(form, name, method=None):
     return {'form': form, 'name': name, 'method': method}
+
+
+@register.filter
+def is_starred(teacher, student):
+    try:
+        return TeacherStudentRelation.objects.get(
+            teacher=teacher,
+            student=student,
+        ).starred
+    except TeacherStudentRelation.DoesNotExist:
+        return False
