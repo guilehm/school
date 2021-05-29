2754c332bdc853b2f16dc276539a7aee698c8c8e
guilherme <guile.hm@hotmail.com>
2021-05-29 00:10:21 -0300

    improve signals

--- school/core/signals.py
+++ school/core/signals.py
@@ -1,17 +1,17 @@
-from django.db.models.signals import m2m_changed, pre_save, pre_delete
+from django.db.models.signals import post_save, post_delete, m2m_changed
 from django.dispatch import receiver
 
 from school.core.models import Teacher, Student, TeacherStudentRelation
 
 
-@receiver(pre_save, sender=TeacherStudentRelation)
+@receiver(post_save, sender=TeacherStudentRelation)
 def create_reverse_relation(sender, **kwargs):
     teacher = kwargs['instance'].teacher
     student = kwargs['instance'].student
     student.teachers.add(teacher.id)
 
 
-@receiver(pre_delete, sender=TeacherStudentRelation)
+@receiver(post_delete, sender=TeacherStudentRelation)
 def delete_reverse_relation(sender, **kwargs):
     teacher = kwargs['instance'].teacher
     student = kwargs['instance'].student
