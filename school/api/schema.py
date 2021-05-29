82f046ab6ef01a22de30d5d80891b3b252f8ca6d
guilherme <guile.hm@hotmail.com>
2021-05-29 12:22:26 -0300

    create queries by username methods

--- school/api/schema.py
+++ school/api/schema.py
@@ -30,11 +30,30 @@ class Query(graphene.ObjectType):
     all_teachers = graphene.List(TeacherType)
     all_students = graphene.List(StudentType)
 
+    teacher_by_username = graphene.Field(
+        TeacherType, username=graphene.String(required=True)
+    )
+    student_by_username = graphene.Field(
+        StudentType, username=graphene.String(required=True)
+    )
+
     def resolve_all_teachers(root, info):
         return Teacher.objects.all()
 
     def resolve_all_students(root, info):
         return Student.objects.all()
+
+    def resolve_teacher_by_username(root, info, username):
+        try:
+            return Teacher.objects.get(username=username)
+        except Teacher.DoesNotExist:
+            return None
+
+    def resolve_student_by_username(root, info, username):
+        try:
+            return Student.objects.get(username=username)
+        except Student.DoesNotExist:
+            return None
 
 
 schema = graphene.Schema(query=Query)
